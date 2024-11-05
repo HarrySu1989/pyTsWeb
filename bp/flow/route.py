a2f13ac3f7b7955fd94e2a9cd091fd4a13123222
hengyi <605879432@qq.com>
2024-11-05 14:22:47 +0800

    优化字符串输入

--- bp/flow/route.py
+++ bp/flow/route.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template
 from decorators import login_required
+import glb.ViewBase as vb
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 bp = Blueprint('flow', __name__, url_prefix='/flow')
 @bp.route('/', methods=['GET', 'POST'])
 @login_required
@@ -8,4 +13,10 @@ def index():
     # html=guide.get_html()
     # html+=guide.item_gx.get_html_body()
     # return vb.get_view(bp, html)
-    return "hello world"
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1200,1000")
+    service = Service('./bp/flow/chromedriver-130.0.6723.91.exe')
+    driver = webdriver.Chrome(options=chrome_options, service=service)
+    driver.get("https://www.baidu.com")
+
+    return vb.get_view(bp, "hello world")
