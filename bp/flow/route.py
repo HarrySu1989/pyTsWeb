568d531f4132d5f47b0bf037f5a3d1a09e926405
hengyi <605879432@qq.com>
2024-11-05 21:50:58 +0800

    优化字符串输入

--- bp/flow/route.py
+++ bp/flow/route.py
@@ -26,7 +26,7 @@ def index():
   <div>
     <label for="example-textbox">工单:</label>
     <input type="text" id="example-textbox" name="example-textbox">
-    <button id="button-flow-begin">开始测试</button>
+    <button id="button-flow-begin">开始</button>
     <script src="/static/jquery-3.6.0.min.js"></script>
     <script src="/static/flow.js"></script>
   </div>
@@ -39,15 +39,22 @@ class ClsTest():
   def __init__(self):
     self.t = None
     self.count = 0
+    self.sq_end=False
     self.log = "请录入工单，点击开始测试"
 
   def get_value(self, req):
     if self.t and self.t.is_alive():
+      if req==0:
+        return "当前流量仪正在测试"
+      elif req==-1:
+        self.sq_end=True
+        return "结束申请"
       return f"{self.log}[{self.count}]"
-    if req:
+    if req==0:
       self.t = threading.Thread(target=self.thred_run)
       self.t.start()
       self.log = "开始"
+      self.sq_end=False
       self.count = 0
     return self.log
 
@@ -56,7 +63,11 @@ class ClsTest():
       self.count += 1
       sleep(1)
       if self.count >20:
-        self.log = "完成"
+        self.log = "测试结束(完成)"
+        return
+      if self.sq_end:
+        self.log = "测试结束(中断)"
+        self.count = 0
         return
 
 
@@ -66,7 +77,7 @@ test = ClsTest()
 @bp.route('/update', methods=['POST'])
 def update():
   data = request.json  # 获取Ajax请求发送的JSON数据
-  req = True if data.get('count') == 0 else False
+  req = data.get('count')
   # response = {'message': 'Success', 'new_content': '这是更新后的内容'}  # 模拟处理并返回新内容
   global test
   response = {'message': 'Error1', 'new_content': f'{test.get_value(req)}'}  # 模拟处理并返回新内容
--- static/flow.js
+++ static/flow.js
@@ -1,28 +1,49 @@
 // script src="/static/flow.js"></script>
+function get_data(count1, heading, button) {
+  $.ajax({
+    url: '/flow/update',  // 请求的URL
+    type: 'POST',  // 请求类型
+    contentType: 'application/json',  // 发送数据的内容类型
+    data: JSON.stringify({count: count1}),  // 发送的数据（这里为空对象，只是示例）
+    success: function (response) {  // 请求成功时的回调函数
+      var log = response.new_content
+      if (log.toString().startsWith("测试结束")) {
+        button.innerHTML = "开始"
+      }else if (log === "当前流量仪正在测试") {
+        button.innerHTML = "结束"
+        alert(log)
+      }
+      heading.innerHTML = log;  // 更新页面内容
+    },
+    error: function (xhr, status, error) {  // 请求失败时的回调函数
+      console.error('Ajax请求失败:', status, error);
+      heading.innerHTML = "Ajax请求失败"
+      button.innerHTML = "开始"
+    }
+  });
+}
+
 $(document).ready(function () {
   $('#button-flow-begin').click(function () {
     var count1 = 0
     var heading = document.getElementById('div-flow-status');
-    heading.innerHTML ="准备开始"  // 更新页面内容
+    var button = document.getElementById('button-flow-begin');
+
+
+    if (button.innerHTML === "结束") {
+      get_data(-1, heading, button)
+      button.innerHTML = "结束申请"
+      return;
+    } else if (button.innerHTML === "开始") {
+      button.innerHTML = "结束"
+    } else if (button.innerHTML === "结束申请") {
+      return;
+    }
     var timer = setInterval(function () {
-      $.ajax({
-        url: '/flow/update',  // 请求的URL
-        type: 'POST',  // 请求类型
-        contentType: 'application/json',  // 发送数据的内容类型
-        data: JSON.stringify({count: count1}),  // 发送的数据（这里为空对象，只是示例）
-        success: function (response) {  // 请求成功时的回调函数
-          var log = response.new_content
-          if (log == "完成") {
-            clearInterval(timer);
-          }
-          heading.innerHTML=log;  // 更新页面内容
-        },
-        error: function (xhr, status, error) {  // 请求失败时的回调函数
-          console.error('Ajax请求失败:', status, error);
-          heading.innerHTML="Ajax请求失败"
-          clearInterval(timer);
-        }
-      });
+      get_data(count1, heading, button)
+      if (button.innerHTML === "开始") {
+        clearInterval(timer);
+      }
       count1 += 1
     }, 1000)
 
