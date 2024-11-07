0488c9abe19432b7f77ac4b8eeb8ac72954d608b
harry <605879432@qq.com>
2024-11-07 16:52:17 +0800

    test

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from waitress import serve
 import app  # 确保这里导入你的 Flask 应用
 
 if __name__ == '__main__':
-    serve(app.app, host='0.0.0.0', port=5001)
+    serve(app.app, host='0.0.0.0', port=5001,threads=10)
