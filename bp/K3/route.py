cd9712ba7b657a59bdac38977e1b931114d4902c
harry <605879432@qq.com>
2025-01-18 16:28:32 +0800

    解决流量仪测试异常问题呢

--- bp/K3/route.py
+++ bp/K3/route.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import os
 import time
 bp = Blueprint('K3', __name__, url_prefix='/K3')
@@ -52,16 +53,63 @@ def index():
       # 1f8c18f9-0061-4b41-ad7d-f69a0797124c-FBILLMATERIALID
       # 1f8c18f9-0061-4b41-ad7d-f69a0797124c-
       # 假设你要定位一个 ID 包含 "partialIdString" 的元素
-      partial_id_string = "FBILLMATERIALID-EDITOR"
-      xpath_expression = f"//*[contains(@id, '{partial_id_string}')]"
-      lb_a = driver.find_element(By.XPATH, xpath_expression)
+
+      lb_a = driver.find_element(By.XPATH, f"//*[contains(@id, 'FBILLMATERIALID-EDITOR')]")
       break
     except:
       print("等待物料编码")
       time.sleep(1)
   if not lb_a:
     return "无法获取物物料编码"
-  lb_a.send_keys("Ts")
-  print("获取物物料编码")
+  # ui-poplistedit-displayname
+  # ui-poplistedit-displayname
+  lb_b=lb_a.find_element(By.CLASS_NAME, f'k-input')
+  print(lb_b)
+  # 使用 JavaScript 执行器更新 span 元素的内容
+  lb_b.send_keys('AOC.100G.OM3-QSFPLC-30-00-00')
+  time.sleep(0.2)
+  lb_b.send_keys(Keys.RETURN)  # 或者使用 Keys.ENTER，它们是等价的
+  time.sleep(0.2)
+  lb_a = driver.find_element(By.XPATH, f"//*[contains(@id, 'FREFRESH_c')]")
+  # print(lb_a.text)
+  lb_a.click()
+  time.sleep(2)
+  lb_a = driver.find_element(By.XPATH, f"//*[contains(@id, 'BILLMENU_TOOLBAR-tbExport')]")
+  lb_a.click()
+  lb_a = None
+  for i in range(30):
+    try:
+      # 1f8c18f9-0061-4b41-ad7d-f69a0797124c-FBILLMATERIALID
+      # 1f8c18f9-0061-4b41-ad7d-f69a0797124c-
+      # 假设你要定位一个 ID 包含 "partialIdString" 的元素
+
+      lb_a = driver.find_element(By.XPATH, f"//*[contains(@id, 'FBTNOK_c')]")
+      break
+    except:
+      print("等待确认按钮")
+      time.sleep(1)
+  if not lb_a:
+    return "无法获取确认按钮"
+
+  lb_a.click()
+  lb_a = None
+  for i in range(30):
+    try:
+      # 1f8c18f9-0061-4b41-ad7d-f69a0797124c-FBILLMATERIALID
+      # 1f8c18f9-0061-4b41-ad7d-f69a0797124c-
+      # 假设你要定位一个 ID 包含 "partialIdString" 的元素
+
+      lb_a = driver.find_element(By.XPATH, f"//*[contains(@id, 'FDOWNLOAD_c')]")
+      break
+    except:
+      print("等待下载按钮")
+      time.sleep(1)
+  if not lb_a:
+    return "无法获取下载按钮"
+
+  lb_a.click()
+  #
+
+#
   time.sleep(10000000)
   return s_exe
